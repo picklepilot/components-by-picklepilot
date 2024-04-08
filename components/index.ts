8ba279b6b5290fda953606779a146a16df3f076a
Justin Kaczmar <kaczmar.justin@gmail.com>
2024-04-08 15:38:17 -0500

    task: create index.ts in components

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import '../src/output.css'
+
+export { default as ModalComponent } from './modal/ModalComponent.vue'
+export { default as ModalComponent2 } from './color-picker/ColorPicker.vue'
+export { default as SortableComponent } from './sortable/SortableComponent.vue'
