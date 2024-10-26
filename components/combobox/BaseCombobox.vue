2209369ea5cb7ddb9dff5c6b8d403904a03cde1f
Justin Kaczmar <kaczmar.justin@gmail.com>
2024-10-26 13:06:00 +0000

    refactor: Update BaseCombobox.vue to support nullable selection better

--- components/combobox/BaseCombobox.vue
+++ components/combobox/BaseCombobox.vue
@@ -198,7 +198,7 @@ watch(
     () => props.items,
     () => {
         filteredItems.value = props.items
-        selected.value = props.items[0]
+        selected.value = !props.nullable ? props.items[0] : null
     },
     { deep: true },
 )
