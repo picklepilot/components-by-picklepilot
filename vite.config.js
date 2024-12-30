6ad92ad27193abe634935adc80219d9dd11cf38a
picklepilot <kaczmar.justin@gmail.com>
2024-12-30 12:05:12 -0600

    refactor: add hash to vite build filename for added uniqueness

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
+const hash = Math.floor(Math.random() * 90000) + 10000
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), dts({ include: ['components'] })],
@@ -11,7 +13,8 @@ export default defineConfig({
             entry: resolve(__dirname, 'components/index.ts'),
             name: 'components-by-picklepilot',
             formats: ['es'],
-            fileName: (format) => `components-by-picklepilot.${format}.js`,
+            fileName: (format) =>
+                `components-by-picklepilot.${format}-${hash}.js`,
         },
         emptyOutDir: false,
     },
